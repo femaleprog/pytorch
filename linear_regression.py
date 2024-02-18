33adeeadd3bf181258dda521ba420b212fe5a24f
femal.eprog <tichirrasoukainaa@gmail.com>
2024-02-18 19:40:37 +0100

    correction in file linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -4,9 +4,9 @@ import numpy as np
 from sklearn import datasets
 import matplotlib.pyplot as plt
 
-# 0 ) prepare data 
+# prepare data 
 X_numpy, y_numpy = datasets.make_regression(n_samples=100, n_features=1, random_state=1)
-
+print(y_numpy.shape)
 X = torch.from_numpy(X_numpy.astype(np.float32))
 y = torch.from_numpy(y_numpy.astype(np.float32))
 y = y.view(y.shape[0], 1)
